7d695508919583de5240da26762c420f27f5cddf
qyluo <qyluo1212@foxmail.com>
2017-05-26 11:39:01 +0800

    1.Two Sum Solution.java

---
+++ src/Two_Sum_1/Solution.java
@@ -0,0 +1,23 @@
+package Two_Sum_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by qy_lu on 2017/5/26.
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                break;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
