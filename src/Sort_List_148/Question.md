f5efe8b85c98451f0852cdada6053ba730a6ec6f
qyluo <qyluo1212@foxmail.com>
2017-08-20 19:48:06 +0800

    148. Sort List Solution.java

---
+++ src/Sort_List_148/Question.md
@@ -0,0 +1,2 @@
+### 148. Sort List
+Sort a linked list in O(n log n) time using constant space complexity.
---
+++ src/Sort_List_148/Solution.java
@@ -0,0 +1,58 @@
+package Sort_List_148;
+
+/**
+ * Created by qy_lu on 2017/8/20.
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+
+        return mergeSort(l1, l2);
+    }
+
+    ListNode mergeSort(ListNode l1, ListNode l2) {
+        ListNode p = new ListNode(0), l = p;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                l.next = l1;
+                l1 = l1.next;
+            } else {
+                l.next = l2;
+                l2 = l2.next;
+            }
+            l = l.next;
+        }
+
+        if (l1 != null) {
+            l.next = l1;
+        }
+        if (l2 != null) {
+            l.next = l2;
+        }
+
+        return p.next;
+    }
+}
