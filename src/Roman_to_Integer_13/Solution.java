9f50b0f8a856044e20a273fe22b9765cb1327ed0
qyluo <qyluo1212@foxmail.com>
2017-09-03 17:07:34 +0800

    13. Roman to Integer Solution.java

---
+++ src/Roman_to_Integer_13/Solution.java
@@ -0,0 +1,30 @@
+package Roman_to_Integer_13;
+
+import java.util.HashMap;
+
+/**
+ * Created by qy_lu on 2017/9/3.
+ */
+public class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int sum = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                sum -= map.get(s.charAt(i));
+            } else {
+                sum += map.get(s.charAt(i));
+            }
+        }
+
+        return sum;
+    }
+}
